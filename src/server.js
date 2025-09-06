87e7d586f161f38e86b69c2618bd613e76684ff8
MrFtyoQr <quintana.real.jqr@gmail.com>
2025-09-06 01:54:10 -0600

    Crons

--- src/server.js
+++ src/server.js
@@ -5,13 +5,16 @@ import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 import trascationsRoute from "./routes/transactionsRoute.js"
+import job from "./config/cron.js"
 
 dotenv.config();
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") job.start(); // we only want to start the cron job in production environment
+
 // middleware
-app.use(rateLimiter); 
+app.use(rateLimiter);
 app.use(express.json());
 
 // our custom simple middleware
@@ -22,6 +25,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 // Here we put the PORT number in an .env file for security and if we dont have any number in a .env we force the number 5001
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "ok" });
+});
 
 
 app.use("/api/transactions", trascationsRoute);
