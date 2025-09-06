39b48cb72862653bbdb8a15914f85836f070e093
MrFtyoQr <quintana.real.jqr@gmail.com>
2025-09-06 00:44:05 -0600

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,38 @@
+// const express = require("express")
+import express from "express"
+import dotenv from "dotenv"
+import { initDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+import trascationsRoute from "./routes/transactionsRoute.js"
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(rateLimiter); 
+app.use(express.json());
+
+// our custom simple middleware
+// app.use((req, res, next) => {
+//     console.log("Hey we hit a req, the method is", req.method);
+//     next();
+// })
+
+const PORT = process.env.PORT || 5001;
+// Here we put the PORT number in an .env file for security and if we dont have any number in a .env we force the number 5001
+
+
+app.use("/api/transactions", trascationsRoute);
+
+
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is up and running on PORT:", PORT)
+    })
+})
+
+//Vamos campeon, tu puedes con esta app, pronto haras las propias sin necesidad de ver video tutoriales
+//Confio en mi, tu eres yo, y yo soy tu, animo little kimosabi
